5bcee82400721cc540533aab1b54558fd90331fe
vachan-maker <vachan@tutanota.com>
2025-06-11 20:08:11 +0530

    added more questions

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ with open("riddles.json", "r") as file:
     data = json.load(file)
 
 @app.get("/")
+async def get_all():
+    return data
+
+@app.get("/random")
 async def get_riddle():
-    riddle = random.choice(data)
-    return riddle  
+    return random.choice(data)
